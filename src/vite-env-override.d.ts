bd02e0d8a7404abb5b96f685f18daaaeac231d01
Vylegzhanin Ilya <vylegzhanin.ilja@yandex.ru>
2024-05-06 21:07:15 +0300

    fix: dts

--- src/vite-env-override.d.ts
+++ src/vite-env-override.d.ts
@@ -1,9 +1,4 @@
-declare module "*.svg" {
+declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGElement>>
   export default content
-}
-
-declare module "vite-plugin-eslint" {
-  const eslint: any
-  export default eslint
 }
